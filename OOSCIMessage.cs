a5577ca3ca71b397131eb96b04543444ac6ed0d6
andysheen <andymsheen@gmail.com>
2024-01-23 15:51:10 +0000

    Update OOSCIMessage.cs

--- OOSCIMessage.cs
+++ OOSCIMessage.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class OOSCIMessage
+public class OOCSIMessage
 {
 
     //---------These parameters are always present-----------//
